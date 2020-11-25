f8ec3525f19c6acf07342f29f1e8ac4f83a89182
lizzyGabriel <60293465+lizzyGabriel@users.noreply.github.com>
2020-11-25 11:59:37 +0100

    Create problem_two.py

---
+++ problem_two.py
@@ -0,0 +1,14 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    symbol = '#'
+    for i in range(1, n+1):
+        print(('#' * i).rjust(n))
+if __name__ == '__main__':
+    n = int(input('enter an integer:'))
+    staircase(n)
